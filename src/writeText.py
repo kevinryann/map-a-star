77b86c1029c6c057d76b77599a16d0a5b6095120
cyn-rus <cyn.rusadi@gmail.com>
2021-04-03 04:05:06 +0700

    udah nyambung

--- src/writeText.py
+++ src/writeText.py
@@ -1,4 +1,18 @@
+from flask import Flask, request, render_template
+import json
 
-def writeText(longitude, latitude) :
-    with open("../coordinate.txt",'w') as file :
-        file.writelines("%f, %f" % (longitude, latitude))
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def writeText() :
+    if request.method == "POST":
+        data = request.data.decode("utf8")
+        jsonData = json.loads(data)
+        longitude = jsonData["longitude"]
+        latitude = jsonData["latitude"]
+        with open("coordinate.txt",'w') as file :
+            file.writelines("%f, %f" % (longitude, latitude))
+    return render_template("coordinate.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
