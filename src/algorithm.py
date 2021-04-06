65fd2ca7e6dc2637c0551cef3b741f7eb7639e5f
cyn-rus <cyn.rusadi@gmail.com>
2021-04-06 22:40:43 +0700

    backtrack a*

--- src/algorithm.py
+++ src/algorithm.py
@@ -5,14 +5,16 @@ def aStar(start, end):
     cost = 0 # the total cost from start point to end point
     curr = start # initialize the current path to be the starting point
     found = True # used if there's no path to be found
-    visited = []
+    visited = [] # for backtracking purposes
 
     while (not curr.isSame(end)): # loop until the current point = end point
-        adjacent = [x for x in curr.route] # list of adjacent points
+        adjacent = [x for x in curr.route if x not in visited] # list of adjacent points
         findCost = [(99999*99999) for y in range(len(adjacent))] # array that will be filled with the distance between 2 points, the number 99999*99999 is used to avoid big distance numbers
         if not any(adjacent): # if adjacent is empty
-            # path.pop()
-            # continue
+            visited.append(path[len(path)-1]) # to state that the point can no longer be used
+            path.pop() # remove the path that can't be used anymore
+            if len(path) != 0: # if there might still be a way
+                continue # go back to the start of the loop
             found = False # to indicate there aren't any paths
             break # end the loop
 
